7b8e930246599a66305eb8ec8469f27d135b21d9
Zhandos Medetov <zhandosmedetov@gmail.com>
2022-05-17 18:03:38 +0600

    ll2 - dependency injection, dyadya bob

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,16 @@ import (
 
 	todo "github.com/zhandosmd/golang-todo"
 	"github.com/zhandosmd/golang-todo/pkg/handler"
+	"github.com/zhandosmd/golang-todo/pkg/repository"
+	"github.com/zhandosmd/golang-todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
+	srv := new(todo.Server)
 	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
