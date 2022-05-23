79cb5971b4742764753612261cb0e5c790db10ef
Zhandos Medetov <zhandosmedetov@gmail.com>
2022-05-23 14:07:11 +0600

    ll5 - connection with database postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	_ "database/sql"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	todo "github.com/zhandosmd/golang-todo"
 	"github.com/zhandosmd/golang-todo/pkg/handler"
 	"github.com/zhandosmd/golang-todo/pkg/repository"
@@ -10,12 +15,40 @@ import (
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") // directory name
+	viper.SetConfigName("config")  // file name
+
+	return viper.ReadInConfig()
 }
